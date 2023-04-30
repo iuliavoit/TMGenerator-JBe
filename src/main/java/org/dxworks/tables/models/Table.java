39e921b336b1d0b0dd11a86edf186f7f076fa4c4
iuliavoit <iuliavoit13@yahoo.com>
2023-04-30 12:04:15 +0300

    current state

--- src/main/java/org/dxworks/tables/models/Table.java
+++ src/main/java/org/dxworks/tables/models/Table.java
@@ -2,15 +2,16 @@ package org.dxworks.tables.models;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import dtos.ColumnDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Data
 @NoArgsConstructor
@@ -20,72 +21,48 @@ public class Table<T> {
     public String name;
     public List<Column<T, ?>> columns;
 
-   /* public List<String> generateJson(Table<T> table) {
-        List<String> outputJsons = new ArrayList<>();
-        for (Column<T, ?> column : table.getColumns()) {
-            JSONObject columnJson = createColumnObject(column);
-            outputJsons.add(columnJson.toString());
+    public String generateJson() {
+        List<ColumnDTO> columnDTOs = new ArrayList<>();
+        for (Column<T, ?> column : columns) {
+            ColumnDTO columnDTO = createColumnDTO(column);
+            columnDTOs.add(columnDTO);
         }
-        return outputJsons;
+
+        Map<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put("name", name);
+        jsonMap.put("columns", columnDTOs);
+
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(ColumnDTO.class, new ColumnDTOTypeAdapter())
+                .disableHtmlEscaping()
+                .setPrettyPrinting()
+                .create();
+
+        return gson.toJson(jsonMap);
     }
 
-    private JSONObject createColumnObject(Column<T, ?> column) {
-        JSONObject columnJson = new JSONObject();
-        if (column.getId() != null) {
-            columnJson.put("id", column.getId());
-        }
-        if (column.getName() != null) {
-            columnJson.put("name", column.getName());
-        }
-        if (column.getNameStyle() != null) {
-            columnJson.put("nameStyle", column.getNameStyle());
-        }
-        if (column.getDescription() != null) {
-            columnJson.put("description", column.getDescription());
-        }
-        if (column.getDescriptionStyle() != null) {
-            columnJson.put("descriptionStyle", column.getDescriptionStyle());
-        }
-        if (column.getSortable() != null) {
-            columnJson.put("sortable", column.getSortable());
-        }
-        if (column.getBorderL() != null) {
-            columnJson.put("borderL", column.getBorderL());
-        }
-        if (column.getBorderR() != null) {
-            columnJson.put("borderR", column.getBorderR());
-        }
-        if (column.getBorderB() != null) {
-            columnJson.put("borderB", column.getBorderB());
-        }
-        if (column.childrenColumns != null) {
-            JSONArray childrenJsons = new JSONArray();
-            for (ColumnBase childColumn : column.getChildrenColumns()) {
-                if (childColumn instanceof Column) {
-                    childrenJsons.put(createColumnObject((Column) childColumn));
-                }
-            }
-            columnJson.put("childrenColumns", childrenJsons);
-        }
-        return columnJson;
-    }*/
-  /* public String generateJsonFromTable() {
-       Gson gson = new GsonBuilder()
-               .registerTypeAdapter(Column.class, new ColumnSerializer())
-               .setPrettyPrinting()
-               .create();
+    private ColumnDTO createColumnDTO(Column<T, ?> column) {
+        ColumnDTO columnDTO = new ColumnDTO();
+        columnDTO.setId(column.getId());
+        columnDTO.setType(column.getType());
+        columnDTO.setName(column.getName());
+        columnDTO.setNameStyle(column.getNameStyle());
+        columnDTO.setDescription(column.getDescription());
+        columnDTO.setDescriptionStyle(column.getDescriptionStyle());
+        columnDTO.setSortable(column.getSortable());
+        columnDTO.setBorderL(column.getBorderL());
+        columnDTO.setBorderR(column.getBorderR());
+        columnDTO.setBorderB(column.getBorderB());
 
-       return gson.toJson(columns);
-   }*/
-   public String generateJson() {
-       Gson gson = new GsonBuilder()
-               .registerTypeAdapter(Column.class, new ColumnSerializer())
-               .setExclusionStrategies(new ColumnExclusionStrategy())
-               .setPrettyPrinting()
-               .create();
+        List<ColumnDTO> childColumnDTOs = new ArrayList<>();
+        for (Column<T, ?> childColumn : column.getChildrenColumns()) {
+            ColumnDTO childColumnDTO = createColumnDTO(childColumn);
+            childColumnDTOs.add(childColumnDTO);
+        }
+        columnDTO.setChildrenColumns(childColumnDTOs);
+        return columnDTO;
+    }
 
-       return gson.toJson(columns);
-   }
 
     public static <T, R> List<Column<T, ?>> extractLeafColumns(Table<T> table) {
         List<Column<T, ?>> leafColumns = new ArrayList<>();
@@ -104,4 +81,6 @@ public class Table<T> {
             }
         }
     }
+
+
 }
